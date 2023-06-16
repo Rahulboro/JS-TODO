31960623220265ce353dd53b1be5dbf89c5c25d9
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-06-16 17:00:08 +0530

    form done

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded',() =>{
+    const form = document.querySelector('.todo-form');
+    const input = document.querySelector('.task-form');
+    const list_todo = document.querySelector('.todo-list');
+
+    form.addEventListener('submit', (p) =>{
+        p.preventDefault();
+
+        const todo = input.value;
+
+        if (!todo) {
+            alert("please fill out the todo");
+            return;
+        } 
+
+        const todo_el = document.createElement('div')
+        todo_el.classList.add('todo')
+        const todo_content = document.createElement('div')
+        
+
+    })
+
+})
