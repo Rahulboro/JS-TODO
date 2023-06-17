2266a1ce3681668474d167ecac7e5ff12d54f8fc
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-06-17 23:20:29 +0530

    edit btn

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ window.addEventListener('DOMContentLoaded',() =>{
         // here we have created the CONTENT div 
         const todo_el = document.createElement('div')
         todo_el.classList.add('content')
+        todo_li.appendChild(todo_el)
 
         // here we have created the child CONTENT of the div 
         const content_input = document.createElement('input');
@@ -26,15 +27,36 @@ window.addEventListener('DOMContentLoaded',() =>{
 
         // appended the child of the CONTENT 
         todo_el.appendChild(content_input);
-        // create the button element 
-        const btn_action = document.createElement('button')
-        btn.action.classList.add('edit-btn')
-        btn_action.type = 'button'
-        btn_action.innerText = "Edit"
+        // create the edit button element 
+        const btn_edit_action = document.createElement('button')
+        btn_edit_action.classList.add('edit-btn')
+        btn_edit_action.type = 'button'
+        btn_edit_action.innerText = "Edit"
         
-        todo_list.appendChild(btn_action);
+        todo_li.appendChild(btn_edit_action);
+        // create the delete button 
+        const btn_dlt_action = document.createElement('button')
+        btn_dlt_action.classList.add('delete')
+        btn_dlt_action.innerText = "Delete"
+        btn_dlt_action.type = 'button';
+        //  append child edit and delete button 
 
+        todo_li.appendChild(btn_edit_action);
+        todo_li.appendChild(btn_dlt_action);
+        
+        input.value = '';
 
+        btn_edit_action.addEventListener = ('click',(p) =>{
+            if (btn_edit_action.innerText.toLowerCase()== 'edit'){
+                btn_edit_action.innerText = "save"
+                content_input.removeAttribute('readonly')
+                content_input.focus();
+            }
+            else {
+                btn_edit_action.innerText ='edit'
+                content_input.setAttribute('readonly','readonly');
+            }
+        })
     })
 
 })
